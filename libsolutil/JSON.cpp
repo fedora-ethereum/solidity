29cf03652740b998e14ebe1ae719256911ceaf26
Peter Lemenkov <lemenkov@gmail.com>
2023-02-08 11:48:38 +0100

    Revert "Stop checking for jsoncpp version"
    This reverts commit e9d8bbb62469d150341c5d13c684e2878aa04a8e.

--- libsolutil/JSON.cpp
+++ libsolutil/JSON.cpp
@@ -32,6 +32,11 @@
 
 using namespace std;
 
+static_assert(
+	(JSONCPP_VERSION_MAJOR == 1) && (JSONCPP_VERSION_MINOR == 9) && (JSONCPP_VERSION_PATCH == 3),
+	"Unexpected jsoncpp version: " JSONCPP_VERSION_STRING ". Expecting 1.9.3."
+);
+
 namespace solidity::util
 {
 
