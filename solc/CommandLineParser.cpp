5fe62eeb31d09c045c5c35add03aac8b7dbd8714
Martin Blicha <martin.blicha@gmail.com>
2023-05-29 18:05:11 +0200

    [SMTChecker]: Update CVC4 to cvc5

--- solc/CommandLineParser.cpp
+++ solc/CommandLineParser.cpp
@@ -860,7 +860,7 @@ General Information)").c_str(),
 		)
 		(
 			g_strModelCheckerSolvers.c_str(),
-			po::value<string>()->value_name("cvc4,eld,z3,smtlib2")->default_value("z3"),
+			po::value<string>()->value_name("cvc5,eld,z3,smtlib2")->default_value("z3"),
 			"Select model checker solvers."
 		)
 		(
